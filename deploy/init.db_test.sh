f01dfbd2ef5a954e3d76700d95d0b5c231f0edd6
Evgeniy Yasnetskiy <evgeniy.yasnetskiy@noveogroup.com>
2023-08-02 20:16:44 +0300

    deploy scripts fix

--- deploy/init.db_test.sh
+++ deploy/init.db_test.sh
@@ -1,5 +1,5 @@
 bin/console ca:cl -e test
-bin/console do:da:dr --force -e test
+bin/console do:da:dr --force --if-exists -e test
 bin/console do:da:cr -e test
 bin/console do:mi:mi -e test --no-interaction
 bin/console app:fix fixtures/all_test.yaml -e test
